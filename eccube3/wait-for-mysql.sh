29912b85371316aa7764cf83b29a994193db549e
kiy0taka <kiy0taka333@gmail.com>
2017-02-13 10:41:19 +0900

    eccube3コンテナの環境変数を出力するように修正

--- eccube3/wait-for-mysql.sh
+++ eccube3/wait-for-mysql.sh
@@ -9,6 +9,8 @@ export AUTH_MAGIC=XjosAXOzO1B3mE0egwQA
 export MAIL_HOST=mailcatcher
 export MAIL_PORT=1025
 
+${ECCUBE_PATH}/exec_env.sh
+
 echo "Waiting for mysql"
 until mysql -h db --password=password -uroot &> /dev/null
 do
@@ -18,7 +20,6 @@ done
 
 
 >&2 echo "MySQL Ready"
-/var/www/exec_env.sh
 php /var/www/eccube_install.php mysql none --skip-createdb --verbose
 chown -R www-data:www-data /var/www
 apache2-foreground
--- eccube3/wait-for-pgsql.sh
+++ eccube3/wait-for-pgsql.sh
@@ -10,13 +10,15 @@ export AUTH_MAGIC=XjosAXOzO1B3mE0egwQA
 export MAIL_HOST=mailcatcher
 export MAIL_PORT=1025
 
+
+${ECCUBE_PATH}/exec_env.sh
+
 until psql -h db -U "cube3_dev_user" -d "template1" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "Postgres is up - executing command"
-${ECCUBE_PATH}/exec_env.sh
 php ${ECCUBE_PATH}/eccube_install.php pgsql none --skip-createdb --verbose
 chown -R www-data:www-data ${ECCUBE_PATH}
 apache2-foreground
